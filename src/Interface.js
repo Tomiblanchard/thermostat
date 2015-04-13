245f8518b4c4e4529a1f6ca2a1f75e837f1447ad
Tomiblanchard <tomiblanchard@gmail.com>
2015-04-13 18:05:38 +0100

    can change temperature

--- src/Interface.js
+++ src/Interface.js
@@ -6,6 +6,15 @@ function refreshTemperature() {
   document.getElementById("temperature").innerHTML = thermostat.temperature;
   }
 
+function switchPowerSave() {
+  if(document.getElementById("powersave").checked) {
+    thermostat.powerSaveOn();
+  }
+  else {
+    thermostat.powerSaveOff();
+  }
+}
+
 function incrementUp() {
   thermostat.incrementUp();
   refreshTemperature();
