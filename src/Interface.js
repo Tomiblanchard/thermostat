5f6ba1b7fe1c8e16d91008274409c644a70574e1
Tomiblanchard <tomiblanchard@gmail.com>
2015-04-13 17:50:02 +0100

    variable temperature

--- src/Interface.js
+++ src/Interface.js
@@ -1,5 +1,17 @@
 thermostat = new Thermostat;
+
 document.getElementById("temperature").innerHTML = thermostat.temperature;
+
 function refreshTemperature() {
   document.getElementById("temperature").innerHTML = thermostat.temperature;
+  }
+
+function incrementUp() {
+  thermostat.incrementUp();
+  refreshTemperature();
+  }
+
+function incrementDown() {
+  thermostat.incrementDown();
+  refreshTemperature();
   }
